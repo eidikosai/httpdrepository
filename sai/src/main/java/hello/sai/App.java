243526c4897e7ab49f684ec3d5b91ffdad53a468
root <root@chefmq.eidikointernal.com>
2017-11-16 15:33:52 -0500

    httpd

---
+++ sai/src/main/java/hello/sai/App.java
@@ -0,0 +1,13 @@
+package hello.sai;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
